142580a6489b07f5a12cf1db003c998b5acf65a5
Asjad <asjad7zia@gmail.com>
2026-01-15 11:19:13 +0530

    Add Google Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ScrollToHashElement from "./components/ScrollToHashElement.tsx"
+import ReactGA from "react-ga"
 
 import Index from "./pages/Index";
 import Overview from "./pages/Overview";
@@ -31,6 +32,9 @@ import FunctionalGroupExperiment from "./pages/experiments/functionalgroups";
 import AcidBaseTitrationExperiment12 from "./pages/experiments/AcidBaseTitrationExperiment12.tsx";
 import { Analytics } from "@vercel/analytics/react";
 import NotFound from "./pages/NotFound";
+
+const TRACKING_ID = "G-MBWK7ZQC68";
+ReactGA.initialize(TRACKING_ID)
 
 const queryClient = new QueryClient();
 
